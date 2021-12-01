d4023dc0ab73acecd68ecac794a9172d27e1c1fa
NateBurd99 <burdick.nate@gmail.com>
2021-11-30 17:02:46 -0700

    Added Tutor View + Tutor Message Screen TODO John

---
+++ lib/views/message_view.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'dashboards/dashboard_tutor.dart';
+
+
+class MessageScreen extends StatefulWidget {
+  const MessageScreen({Key? key}) : super(key: key);
+
+  @override
+  _MessageScreenState createState() => _MessageScreenState();
+}
+
+class _MessageScreenState extends State<MessageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      //TODO moveToChatRoomListView
+    );
+  }
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.black12,
+      title: Text("Messages"),
+      leading: Builder(
+        builder: (BuildContext context) {
+          return IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {  Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DashboardTutorPage()),
+            );},
+            tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          );
+        },
+      ),
+      actions: [
+      Image(
+      image: AssetImage('assets/unco-bear-letter-logo.png'),
+      )
+        ],
+    );
+  }
+
