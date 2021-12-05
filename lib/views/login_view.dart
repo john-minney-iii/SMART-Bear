5bfbb109c2ed205a99afe3b8fed4e5bee982b5db
john-minney-iii <jminneyiii@gmail.com>
2021-12-05 15:13:19 -0700

    Added logged in check and splash screens

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -20,89 +20,99 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: globalAppBar(context, 'Login', false, false),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                const SizedBox(height: 60.0),
-                Image.asset(
-                  'assets/unco-bear-logo.png',
-                  height: 125.0,
-                  width: 125.0,
-                ),
-                const Text('SMART BEAR App',
-                    style: TextStyle(color: Colors.black, fontSize: 24.0)),
-                Form(
-                  key: _formKey,
+    return FutureBuilder(
+      future: _checkUserLogin(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+              backgroundColor: Colors.white,
+              appBar: globalAppBar(context, 'Login', false, false),
+              body: SingleChildScrollView(
+                child: Center(
                   child: Column(
                     children: <Widget>[
-                      Text('Invalid Email or Password. Please try again.',
-                          style: TextStyle(
-                              color: (_loginFail) ? Colors.red : Colors.white)),
-                      Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: TextFormField(
-                          controller: _emailController,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Email',
-                              hintText: 'Enter a valid email'),
-                          validator: (value) {
-                            // TODO: email validation
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter a valid email';
-                            }
-                            return null;
-                          },
+                      const SizedBox(height: 60.0),
+                      Image.asset(
+                        'assets/unco-bear-logo.png',
+                        height: 125.0,
+                        width: 125.0,
+                      ),
+                      const Text('SMART BEAR App',
+                          style: TextStyle(color: Colors.black, fontSize: 24.0)),
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: <Widget>[
+                            Text('Invalid Email or Password. Please try again.',
+                                style: TextStyle(
+                                    color: (_loginFail) ? Colors.red : Colors.white)),
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: TextFormField(
+                                controller: _emailController,
+                                decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Email',
+                                    hintText: 'Enter a valid email'),
+                                validator: (value) {
+                                  // TODO: email validation
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter a valid email';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: TextFormField(
+                                controller: _passwordController,
+                                decoration: const InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Password',
+                                    hintText: 'Enter a secure password'),
+                                obscureText: true,
+                                validator: (value) {
+                                  // TODO: password validation
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter a password';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            )
+                          ],
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(15.0),
-                        child: TextFormField(
-                          controller: _passwordController,
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Password',
-                              hintText: 'Enter a secure password'),
-                          obscureText: true,
-                          validator: (value) {
-                            // TODO: password validation
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter a password';
-                            }
-                            return null;
-                          },
-                        ),
+                      blueCallToAction('Login', _login),
+                      const SizedBox(height: 50.0),
+                      TextButton(
+                        onPressed: () {
+                          moveToRegisterView(context);
+                        },
+                        child: RichText(
+                            text: const TextSpan(children: [
+                              TextSpan(
+                                  text: 'New User? ',
+                                  style: TextStyle(color: Colors.black)),
+                              TextSpan(
+                                  text: 'Create new Account.',
+                                  style: TextStyle(
+                                      color: Colors.blue,
+                                      decoration: TextDecoration.underline,
+                                      fontWeight: FontWeight.bold))
+                            ])),
                       )
                     ],
                   ),
                 ),
-                blueCallToAction('Login', _login),
-                const SizedBox(height: 50.0),
-                TextButton(
-                  onPressed: () {
-                    moveToRegisterView(context);
-                  },
-                  child: RichText(
-                      text: const TextSpan(children: [
-                    TextSpan(
-                        text: 'New User? ',
-                        style: TextStyle(color: Colors.black)),
-                    TextSpan(
-                        text: 'Create new Account.',
-                        style: TextStyle(
-                            color: Colors.blue,
-                            decoration: TextDecoration.underline,
-                            fontWeight: FontWeight.bold))
-                  ])),
-                )
-              ],
-            ),
-          ),
-        ));
+              ));
+        }
+        return const Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(child: CircularProgressIndicator()));
+      }
+    );
   }
 
   void _login() async {
@@ -114,6 +124,12 @@ class _LoginViewState extends State<LoginView> {
           _loginFail = true;
         });
       }
+    }
+  }
+
+  Future<void> _checkUserLogin() async {
+    if (isUserAuth()) {
+      moveToUserDashboardReplacement(context);
     }
   }
 }
