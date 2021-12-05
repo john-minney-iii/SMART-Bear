f149f063ce2494cbddee52198901e800b0ed8403
john-minney-iii <jminneyiii@gmail.com>
2021-12-04 21:06:26 -0700

    Better chatroom names

--- lib/models/chatroom.dart
+++ lib/models/chatroom.dart
@@ -2,21 +2,28 @@ class ChatRoom {
   String id;
   List<String> userIds;
   bool isOpen;
+  String subject;
 
-  ChatRoom({required this.id, required this.userIds, required this.isOpen});
+  ChatRoom(
+      {required this.id,
+      required this.userIds,
+      required this.isOpen,
+      required this.subject});
 
   factory ChatRoom.fromJson(Map<String, dynamic> json) {
     return ChatRoom(
         id: json['Id'],
         userIds: List.from(json['Users']),
-        isOpen: json['IsOpen']);
+        isOpen: json['IsOpen'],
+        subject: json['Subject']);
   }
 
   Map<String, dynamic> getJson() {
     return {
       'Id': id,
       'Users': [userIds[0], userIds[1]],
-      'IsOpen': isOpen
+      'IsOpen': isOpen,
+      'Subject': subject
     };
   }
 }
