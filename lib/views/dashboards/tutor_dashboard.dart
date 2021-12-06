8c91d4a21c7e3745bb34de427129306a2389433f
john-minney-iii <jminneyiii@gmail.com>
2021-12-06 08:30:14 -0700

    Updated tutor dashboard

--- lib/views/dashboards/tutor_dashboard.dart
+++ lib/views/dashboards/tutor_dashboard.dart
@@ -15,185 +15,200 @@ class TutorDashboard extends StatefulWidget {
 }
 
 class _TutorDashboardState extends State<TutorDashboard> {
+  Color _boxColor = const Color(0xff173f5f);
+  Color _borderColor = const Color(0xFFFFB300);
+  Color _textIconColor = Colors.white;
+  final _borderWidth = 3.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(30.0),
+            child: AppBar(
+              automaticallyImplyLeading: false,
+              title: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Want to become a tutor or SI leader?  ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                    InkWell(
+                        child: Text('Click Here',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15.0,
+                              decoration: TextDecoration.underline,
+                            )),
+                        onTap: () => launch(
+                            'https://www.unco.edu/tutoring/resources-forms/become-a-peer-tutor-or-si-leader.aspx')),
+                  ]),
+              backgroundColor: Colors.amber,
+            )),
         body: Column(
-      children: <Widget>[
-        const SizedBox(height: 40.0),
-        Row(crossAxisAlignment: CrossAxisAlignment.end, children: <Widget>[
-          Container(
-            margin: EdgeInsets.fromLTRB(20.0, 10.0, 30.0, 10.0),
-            child: Image(
-              image: AssetImage('assets/unco-bear-letter-logo.png'),
-              //width: 80,
-              //height: 80,
-              width: MediaQuery.of(context).size.width * 0.15,
-              height: MediaQuery.of(context).size.width * 0.15,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.fromLTRB(20.0, 10.0, 30.0, 10.0),
+                      child: Image(
+                        image: AssetImage('assets/unco-bear-letter-logo.png'),
+                        //width: 80,
+                        //height: 80,
+                        width: MediaQuery.of(context).size.width * 0.15,
+                        height: MediaQuery.of(context).size.width * 0.15,
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.20,
+                      height: MediaQuery.of(context).size.width * 0.10,
+                      margin: EdgeInsets.fromLTRB(20.0, 10.0, 30.0, 10.0),
+                      child: Text('Welcome, Student!',
+                          style: TextStyle(
+                            letterSpacing: 1.0,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    )
+                  ]),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * 0.20,
-            height: MediaQuery.of(context).size.width * 0.10,
-            margin: EdgeInsets.fromLTRB(20.0, 10.0, 30.0, 10.0),
-            child: Text('Welcome, Tutor!',
-                style: TextStyle(
-                  letterSpacing: 1.0,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                )),
-          ),
-          Center(
-            child: TextButton(
-                onPressed: () async {
-                  await signOutCurrentUser();
-                  moveToLoginViewReplacement(context);
-                },
-                child: Text('Logout', style: TextStyle(color: Colors.black))),
-          )
-        ]),
-        Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
-            child: Divider(color: Colors.black),
-          ),
-        ]),
-        Column(children: <Widget>[
-          Container(
-              margin: EdgeInsets.fromLTRB(25.0, 10.0, 0.0, 0.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text('Tutor Dashboard',
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1.0,
-                    )),
-              )),
-        ]),
-        Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.45,
-                    height: MediaQuery.of(context).size.width * 0.25,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueGrey, width: 4.0),
-                    ),
-                    child: RaisedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => AskAQuestionView()),
-                        );
-                      },
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const <Widget>[
-                            Text('Ask A Question',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                )),
-                            Icon(
-                              Icons.help,
-                              size: 50.0,
-                            ),
-                          ]),
-                      color: Colors.lightBlue[100],
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.45,
-                    height: MediaQuery.of(context).size.width * 0.25,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blueGrey, width: 4.0),
-                    ),
-                    child: RaisedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => StudentFAQView()),
-                        );
-                      },
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const <Widget>[
-                            Text('FAQ',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                )),
-                            Icon(
-                              Icons.mail,
-                              size: 50.0,
-                            ),
-                          ]),
-                      color: Colors.lightBlue[100],
-                    ),
-                  ),
-                ]),
-          ),
-        ]),
-        Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
-            child: Divider(color: Colors.black),
-          ),
-        ]),
-        Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-            width: MediaQuery.of(context).size.width * 0.90,
-            height: MediaQuery.of(context).size.width * 0.15,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.blueGrey, width: 4.0),
-            ),
-            child: RaisedButton.icon(
-              onPressed: () async {
-                moveToChatRoomListView(context);
-              },
-              icon: Icon(
-                Icons.drafts_outlined,
-                size: 50.0,
+            Column(children: <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+                child: Divider(color: Colors.black),
               ),
-              label: Text('My Conversations'),
-              color: Colors.lightBlue[100],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-            width: MediaQuery.of(context).size.width * 0.90,
-            height: MediaQuery.of(context).size.width * 0.15,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.blueGrey, width: 4.0),
-            ),
-            child: RaisedButton.icon(
-              onPressed: () async {
-                final url =
-                    'https://www.unco.edu/tutoring/pdf/night-time-drop-in-course-offerings.pdf';
-                if (await canLaunch(url)) {
-                  await launch(
-                    url,
-                    forceSafariVC: false,
-                  );
-                } else {
-                  throw 'Could not launch $url';
-                }
-              },
-              icon: Icon(
-                Icons.nights_stay,
-                size: 50.0,
+            ]),
+            Column(children: <Widget>[
+              Container(
+                  margin: EdgeInsets.fromLTRB(25.0, 10.0, 0.0, 0.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text('Tutor Dashboard',
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.0,
+                        )),
+                  )),
+            ]),
+            Column(children: <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.45,
+                        height: MediaQuery.of(context).size.width * 0.25,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: _borderColor, width: _borderWidth),
+                        ),
+                        child: RaisedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => AskAQuestionView()),
+                            );
+                          },
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text('Ask A Question',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: _textIconColor)),
+                                Icon(Icons.help,
+                                    size: 50.0, color: _textIconColor),
+                              ]),
+                          color: _boxColor,
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.45,
+                        height: MediaQuery.of(context).size.width * 0.25,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: _borderColor, width: _borderWidth),
+                        ),
+                        child: RaisedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => StudentFAQView()),
+                            );
+                          },
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text('FAQ',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: _textIconColor)),
+                                Icon(Icons.mail,
+                                    size: 50.0, color: _textIconColor),
+                              ]),
+                          color: _boxColor,
+                        ),
+                      ),
+                    ]),
               ),
-              label: Text('After Hours Support'),
-              color: Colors.lightBlue[100],
-            ),
-          ),
-        ]),
-      ],
-    ));
+            ]),
+            Column(children: <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+                child: Divider(color: Colors.black),
+              ),
+            ]),
+            Column(children: <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                width: MediaQuery.of(context).size.width * 0.90,
+                height: MediaQuery.of(context).size.width * 0.15,
+                decoration: BoxDecoration(
+                  border: Border.all(color: _borderColor, width: _borderWidth),
+                ),
+                child: RaisedButton.icon(
+                  onPressed: () async {
+                    moveToChatRoomListView(context);
+                  },
+                  icon: Icon(Icons.drafts_outlined,
+                      size: 50.0, color: _textIconColor),
+                  label: Text('My Conversations',
+                      style: TextStyle(color: _textIconColor)),
+                  color: _boxColor,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                width: MediaQuery.of(context).size.width * 0.90,
+                height: MediaQuery.of(context).size.width * 0.15,
+                decoration: BoxDecoration(
+                  border: Border.all(color: _borderColor, width: _borderWidth),
+                ),
+                child: RaisedButton.icon(
+                  onPressed: () async {
+                    await signOutCurrentUser();
+                    moveToLoginViewReplacement(context);
+                  },
+                  icon: Icon(IconData(0xe3b3, fontFamily: 'MaterialIcons'),
+                      size: 50.0, color: _textIconColor),
+                  label:
+                      Text('Logout', style: TextStyle(color: _textIconColor)),
+                  color: _boxColor,
+                ),
+              ),
+            ]),
+          ],
+        ));
   }
 }
